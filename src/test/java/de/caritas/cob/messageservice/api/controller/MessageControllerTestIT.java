78a207e51daa513dbc1bd3e7bb7cd1877d1d1658
Torsten Krohn <torsten.krohn@webatspeed.com>
2022-03-16 09:01:37 +0100

    feat: set user-muted message type

--- src/test/java/de/caritas/cob/messageservice/api/controller/MessageControllerTestIT.java
+++ src/test/java/de/caritas/cob/messageservice/api/controller/MessageControllerTestIT.java
@@ -103,7 +103,7 @@ public class MessageControllerTestIT {
           .imagePreview(RC_ATTACHMENT_IMAGE_PREVIEW);
   private final MessagesDTO MESSAGES_DTO = new MessagesDTO("123", null, RC_GROUP_ID, MESSAGE,
       RC_TIMESTAMP, new UserDTO(RC_USER_ID, "test", "name"), false, new String[0], new String[0],
-      RC_TIMESTAMP, Arrays.array(ATTACHMENT_DTO), FILE_DTO);
+      RC_TIMESTAMP, Arrays.array(ATTACHMENT_DTO), FILE_DTO, null);
   private final String PATH_UPDATE_KEY = "/messages/key?key=";
   private final String PATH_DRAFT_MESSAGE = "/messages/draft";
   private final String QUERY_PARAM_OFFSET = "offset";
