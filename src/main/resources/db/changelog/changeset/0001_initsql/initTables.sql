b0da12c0ffbe5137f0864fef46fa7ccdc574e68d
tkuzynow <tomasz.kuzynowski@gmail.com>
2022-10-28 11:49:49 +0200

    fix: fix mysql incompatibilities

--- src/main/resources/db/changelog/changeset/0001_initsql/initTables.sql
+++ src/main/resources/db/changelog/changeset/0001_initsql/initTables.sql
@@ -3,8 +3,8 @@ CREATE TABLE messageservice.`draftmessage` (
   `user_id` varchar(36) COLLATE utf8_unicode_ci NOT NULL,
   `rc_group_id` varchar(255) NOT NULL,
   `draft_message` longtext COLLATE utf8_unicode_ci NOT NULL,
-  `create_date` datetime NOT NULL DEFAULT UTC_TIMESTAMP(),
-  `update_date` datetime NOT NULL DEFAULT UTC_TIMESTAMP(),
+  `create_date` datetime NOT NULL DEFAULT UTC_TIMESTAMP,
+  `update_date` datetime NOT NULL DEFAULT UTC_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 CREATE SEQUENCE messageservice.sequence_draftmessage
